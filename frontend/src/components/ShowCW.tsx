f476c7021b182bf964b8301ffc487eccb5681e47
Lui <6627294+yablochko8@users.noreply.github.com>
2024-07-06 15:49:52 -0400

    misc

--- frontend/src/components/ShowCW.tsx
+++ frontend/src/components/ShowCW.tsx
@@ -1,5 +1,5 @@
 import { CrossWord, BoolGrid, Clue } from "../data/types"
-import { answerLength } from "../services/answerLength";
+import { answerLength } from "../services/BooomnswerLength";
 
 
 type TileProps = {
